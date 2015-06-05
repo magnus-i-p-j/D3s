65919742c25af7e44240e241a51d322c2f815eb3
unknown <jonnerydma@WMLI051803.groupinfra.com>
2015-06-05 15:58:45 +0200

    ?

---
+++ 3Ds/Specification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3Ds.Core
+{
+    public class Specification
+    {
+    }
+}
---
+++ 3Ds/UnitOfWorkException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Ds.Core
+{
+    [Serializable]
+    public class UnitOfWorkException : Exception
+    {
+        public UnitOfWorkException() { }
+        public UnitOfWorkException(string message) : base(message) { }
+        public UnitOfWorkException(string message, Exception inner) : base(message, inner) { }
+        protected UnitOfWorkException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
